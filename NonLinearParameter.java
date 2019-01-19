7bb34506ead486200266c6d274755d211edf938c
ff2084 <46849547+ff2084@users.noreply.github.com>
2019-01-19 15:06:09 -0500

    Add files via upload

---
+++ NonLinearParameter.java
@@ -0,0 +1,96 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.glotaran.core.ui.visualmodelling.nodes.dataobjects;
+
+import org.glotaran.core.models.tgm.KinPar;
+
+/**
+ * @author lsp
+ */
+public class NonLinearParameter extends AbstractParameterDO implements Comparable<NonLinearParameter>{
+
+    private Double start;
+    private Boolean fixed;
+    private Boolean constrained;
+    private Double maximum;
+    private Double minimum;
+
+    public NonLinearParameter() {
+        start = new Double(0.0);
+        fixed = false;
+        constrained = false;
+        maximum = new Double(0.0);
+        minimum = new Double(0.0);
+
+    }
+
+    public NonLinearParameter(Double irfPar, Boolean fixedPar) {
+        start = irfPar;
+        fixed = fixedPar;
+        constrained = false;
+        maximum = new Double(0.0);
+        minimum = new Double(0.0);
+
+    }
+
+    public NonLinearParameter(KinPar param) {
+        start = param.getStart();
+        fixed = param.isFixed();
+        constrained = param.isConstrained();
+        maximum = param.getMax();
+        minimum = param.getMin();
+    }
+
+    public Double getStart() {
+        return start;
+    }
+
+    public void setStart(Double value) {
+        Double oldStart = start;
+        this.start = value;
+        fire("start", oldStart, start);
+    }
+
+    public Boolean isFixed() {
+        return fixed;
+    }
+
+    public void setFixed(Boolean value) {
+        Boolean oldFixed = fixed;
+        this.fixed = value;
+        fire("fixed", oldFixed, fixed);
+    }
+
+    public Boolean isConstrained() {
+        return constrained;
+    }
+
+    public void setConstrained(Boolean value) {
+        this.constrained = value;
+    }
+
+    public Double getMaximum() {
+        return maximum;
+    }
+
+    public void setMaximum(Double value) {
+        this.maximum = value;
+    }
+
+    public Double getMinimum() {
+        return minimum;
+    }
+
+    public void setMinimum(Double value) {
+        this.minimum = value;
+    }
+
+    @Override
+    public int compareTo(NonLinearParameter nonLinearParameter) {
+        double value = nonLinearParameter.getStart();
+        //sort in descening order (from large to small)
+        return this.start > value ? -1 : this.start < value ? 1 : 0;
+    }
+}
---
+++ NonLinearParametersKeys.java
@@ -0,0 +1,139 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.glotaran.core.ui.visualmodelling.nodes.dataobjects;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.glotaran.core.models.tgm.KinPar;
+import org.glotaran.core.ui.visualmodelling.nodes.CohspecParametersSubNode;
+import org.glotaran.core.ui.visualmodelling.nodes.ParametersSubNode;
+import org.openide.nodes.Children;
+import org.openide.nodes.Node;
+
+/**
+ *
+ * @author slapten
+ */
+public class NonLinearParametersKeys extends Children.Keys {
+
+    private List<NonLinearParameter> parameters;
+    private String nodeType;
+
+    public NonLinearParametersKeys(int paramNum) {
+        nodeType = "nonlinearParameter";
+        parameters = new ArrayList<NonLinearParameter>();
+        for (int i = 0; i < paramNum; i++) {
+            parameters.add(new NonLinearParameter());
+        }
+    }
+
+    public NonLinearParametersKeys(List<KinPar> paramList) {
+        nodeType = "nonlinearParameter";
+        parameters = new ArrayList<>();
+        if (paramList != null) {
+            for (KinPar paramList1 : paramList) {
+                parameters.add(new NonLinearParameter(paramList1));
+            }
+        }
+    }
+    
+    public NonLinearParametersKeys(List<KinPar> paramList, String nodetype) {
+        nodeType = nodetype;
+        parameters = new ArrayList<>();
+        if (paramList != null) {
+            for (KinPar paramList1 : paramList) {
+                parameters.add(new NonLinearParameter(paramList1));
+            }
+        }
+    }
+
+    @Override
+    protected void addNotify() {
+        setKeys(parameters);
+    }
+
+    @Override
+    protected Node[] createNodes(Object key) {
+        if (nodeType.equalsIgnoreCase("XPMCohSpecNode")) {
+            return new Node[]{new CohspecParametersSubNode((NonLinearParameter) key, "XPM")};
+        } else {
+            return new Node[]{new ParametersSubNode((NonLinearParameter) key)};
+        }
+    }
+    
+    
+    public void addObj(NonLinearParameter objToAdd){
+        addObj(objToAdd,"nonlinearParameter");
+    }
+
+    public void addObj(NonLinearParameter objToAdd, String nodetype) {
+        nodeType = nodetype;
+        if (parameters != null) {
+            parameters.add(objToAdd);
+        } else {
+            parameters = new ArrayList<>();
+            parameters.add(objToAdd);
+        }
+        setKeys(parameters);
+    }
+
+    public void removeParams(int num) {
+//remove num last components
+        if (parameters != null) {
+            if (parameters.size() < num) {
+                parameters.clear();
+                parameters.add(new NonLinearParameter());
+            } else {
+                for (int i = 0; i < num; i++) {
+                    parameters.remove(parameters.size() - 1);
+                }
+            }
+        }
+        setKeys(parameters);
+    }
+
+    public void addDefaultObj(int numObj){
+        addDefaultObj(numObj, "nonlinearParameter");
+    }
+            
+    public void addDefaultObj(int numObj, String nodetype) {
+        nodeType = nodetype;
+        if (parameters != null) {
+            for (int i = 0; i < numObj; i++) {
+                parameters.add(new NonLinearParameter());
+            }
+        } else {
+            parameters = new ArrayList<>();
+            for (int i = 0; i < numObj; i++) {
+                parameters.add(new NonLinearParameter());
+            }
+        }
+        setKeys(parameters);
+    }
+
+    @Override
+    public boolean remove(Node[] arg0) {
+        for (Node arg01 : arg0) {
+            ParametersSubNode node = (ParametersSubNode) arg01;
+            parameters.remove(node.getDataObj());
+            setKeys(parameters);
+        }
+        return true;
+    }
+
+    protected void backToParams() {
+        setKeys(parameters);
+    }    
+    
+    public void sortParams() {
+        Collections.sort(parameters);                   
+        setKeys(parameters);
+        for (NonLinearParameter nlp : parameters) {            
+            nlp.fire("start", null, nlp.getStart());
+            nlp.fire("fixed", null, nlp.isFixed());            
+        }
+    }
+}
